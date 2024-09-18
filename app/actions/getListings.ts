5488b89a7718eeb3c539abf195d34b73e20906e2
Tần <doantan1802@gmail.com>
2024-09-18 13:10:59 +0700

    Filters modal

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,18 +2,79 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListing(
     params: IListingsParams
 ) {
     try {
-        const { userId } = params;
+        const {
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category
+        } = params;
 
-        let query = {};
+        let query: any = {};
 
         if (userId) {
             query.userId = userId;
+        }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+        
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue; 
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate }, // Đặt chỗ có ngày kết thúc sau hoặc bằng với ngày bắt đầu mà người dùng chọn
+                                startDate: { lte: startDate }, // Đặt chỗ có ngày bắt đầu trước hoặc bằng với ngày bắt đầu mà người dùng chọn
+                            },
+                            {
+                                startDate: { lte: endDate }, // Đặt chỗ có ngày bắt đầu trước hoặc bằng với ngày kết thúc mà người dùng chọn
+                                endDate: { gte: endDate } // Đặt chỗ có ngày kết thúc sau hoặc bằng với ngày kết thúc mà người dùng chọn
+                            }
+                        ]
+                    }
+                }
+            }
         }
 
         const listings = await prisma.listing.findMany({
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,10 +1,58 @@
 'use client';
 
-import { BiSearch } from 'react-icons/bi'
+import { useMemo } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { useSearchParams } from 'next/navigation';
+import { differenceInDays } from 'date-fns';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useCountries from '@/app/hooks/useCountries';
 
 const Search = () => {
+    const searchModal = useSearchModal();
+    const params = useSearchParams();
+    const { getByValue } = useCountries();
+
+    const locationValue = params?.get('locationValue');
+    const startDate = params?.get('startDate');
+    const endDate = params?.get('endDate');
+    const guestCount = params?.get('guestCount');
+
+    const locationLabel = useMemo(() => {
+        if (locationValue) {
+            return getByValue(locationValue as string)?.label;
+        }
+
+        return 'Anywhere';
+    }, [getByValue, locationValue]);
+
+    const durationLabel = useMemo(() => {
+        if (startDate && endDate) {
+            const start = new Date(startDate as string);
+            const end = new Date(endDate as string);
+            let diff = differenceInDays(end, start);
+
+            if (diff === 0) {
+                diff = 1;
+            }
+
+            return `${diff} Days`;
+        }
+
+        return 'Any week';
+    }, [startDate, endDate]);
+
+    const guestsLabel = useMemo(() => {
+        if (guestCount) {
+            return `${guestCount} Guests`;
+        }
+
+        return 'Add Guests';
+    }, [guestCount]);
+
     return ( 
         <div
+            onClick={searchModal.onOpen}
             className="
                 border-[1px]
                 w-full
@@ -32,7 +80,7 @@ const Search = () => {
                         px-6
                     "
                 >
-                    Anywhere
+                    {locationLabel}
                 </div>
                 <div
                     className="
@@ -46,7 +94,7 @@ const Search = () => {
                         text-center
                     "
                 >
-                    Any week
+                    {durationLabel}
                 </div>
                 <div
                     className="
@@ -60,7 +108,7 @@ const Search = () => {
                         gap-3
                     "   
                 >
-                    <div className="hidden sm:block">Add Guests</div>
+                    <div className="hidden sm:block">{guestsLabel}</div>
                     <div
                         className="
                             p-2
